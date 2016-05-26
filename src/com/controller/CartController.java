776b50e63698a9d9c3d1f2c6d8d6bf9ecd678dd2
Suresh Chaudhari <chaudharisuresh997@gmail.com>
2016-05-26 19:59:34 +0530

    Add files via upload

--- src/com/controller/CartController.java
+++ src/com/controller/CartController.java
@@ -23,14 +23,14 @@ import com.model.*;
 @WebServlet("/Cart")
 public class CartController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CartController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CartController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -38,29 +38,46 @@ public class CartController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession sessionObj=request.getSession();
 		com.google.gson.Gson gsonOb=new Gson();
-		
-		Type collectionType = new TypeToken<List<Product>>(){}.getType();	
-		
-	String s=request.getParameter("paramsMap");
-	
-	List<Product> cartObj=gsonOb.fromJson(s, collectionType);
-	if(sessionObj.getAttribute("cart")!=null)
-	{
-		List<Product> carts=(List<Product>) sessionObj.getAttribute("cart");
-		
-		carts.add(cartObj.get(0));
-		sessionObj.setAttribute("cart", carts);
-		
-	}
-	else
-	{
-	sessionObj.setAttribute("cart", cartObj);
-	}
-	PrintWriter pw=response.getWriter();
-	pw.println(s);
-	pw.close();
-	}
 
+		Type collectionType = new TypeToken<List<Product>>(){}.getType();	
+
+		String s=request.getParameter("paramsMap");
+		String removeItem=request.getParameter("remove");
+		if(removeItem!=null)
+		{
+			List<Product> carts=(List<Product>) sessionObj.getAttribute("cart");
+			for(int index=0;index<carts.size();index++)
+			{
+				if(carts.get(index).getProductName().equals(removeItem))
+				{
+					carts.remove(index);
+					sessionObj.setAttribute("cart", carts);
+					response.sendRedirect("/AdJava/ViewCart?id=");
+				}
+			}
+			
+		}
+		if(s!=null)
+		{
+			List<Product> cartObj=gsonOb.fromJson(s, collectionType);
+			if(sessionObj.getAttribute("cart")!=null)
+			{
+				List<Product> carts=(List<Product>) sessionObj.getAttribute("cart");
+
+				carts.add(cartObj.get(0));
+				sessionObj.setAttribute("cart", carts);
+
+			}
+			else
+			{
+				sessionObj.setAttribute("cart", cartObj);
+			}
+
+			PrintWriter pw=response.getWriter();
+			pw.println(s);
+			pw.close();
+		}
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
