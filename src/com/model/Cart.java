d4ded79a5a878122c4c1f6a4445001448c90441a
Suresh Chaudhari <chaudharisuresh997@gmail.com>
2016-05-26 18:13:31 +0530

    Add files via upload

---
+++ src/com/model/Cart.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import java.util.List;
+
+public class Cart
+{
+	public List<Product> products;
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	}
